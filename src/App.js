a64ec4b75762e7ccbcec292ad7b70008cf381b14
dswsd <babdoy123@gmail.com>
2025-07-15 15:21:49 +0900

    gkgk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Test from "./addMod";
+// import Test, {PI} from "./addMod";
+import {PI, Test}from "./addMod";
 
 function App() {
 
   return (
     <div className="App">
      <Test />
+     <h1>원주율: {PI}</h1>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Header extends Component{
+    render(){
+        return(<div>클래스 컴포넌트 Header</div>);
+    }
+}
+
+export default Header;
