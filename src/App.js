6432e1156af1a6c10bef3106ba3436f7dc02728f
dswsd <babdoy123@gmail.com>
2025-07-15 15:25:20 +0900

    lee

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 // import Test, {PI} from "./addMod";
-import {PI, Test}from "./addMod";
+import {PI as p, Test as Ts} from "./addMod";
+import Header from './components/Header';
 
 function App() {
-
+  
   return (
     <div className="App">
-     <Test />
-     <h1>원주율: {PI}</h1>
+      <Ts />
+      <h1>원주율: {p}</h1>
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 class Header extends Component{
     render(){
-        return(<div>클래스 컴포넌트 Header</div>);
+        return(<div className="headerCom">클래스 컴포넌트 Header</div>);
     }
 }
 
